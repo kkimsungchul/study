51ff51f49717d3d7ebf78893ff2820227acc7730
kkimsungchul <kimsc1218@gmail.com>
2022-03-28 14:45:19 +0900

    2022.03.28 1. 파일경로 이동 2. findText 프로젝트 수정

--- 잡다한프로젝트/findText/src/main/java/com/sungchul/main/Main.java
+++ 잡다한프로젝트/findText/src/main/java/com/sungchul/main/Main.java
@@ -1,7 +1,6 @@
 package com.sungchul.main;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.util.*;
 
 public class Main {
@@ -16,17 +15,20 @@ public class Main {
         int count = 0;
         StringBuffer stringBuffer = new StringBuffer();
         String line = null;
-        String killUser;
+        String killUser=null;
         //채팅에 참여한 사용자 목록
         String memberName;
         ArrayList<String> memberList = new ArrayList<String>();
         //사용자명,채팅횟수 기록
         HashMap<String,Integer> talkCountMap = new HashMap<String, Integer>();
+        HashMap<String,Integer> killCountMap = new HashMap<String, Integer>();
+        HashMap<String,String> killUserListMap = new HashMap<String, String>();
 
         int totalLine=0;
 
         int memberCount=0;
         try {
+            //사용자 목록 추출
             br = new BufferedReader(new FileReader(txt));
             while((line=br.readLine()) != null) {
                 int t=line.indexOf("]");
@@ -44,17 +46,35 @@ public class Main {
                     memberCount=0;
                 }
             }
+
+            //사용자세팅
             for(int i=0;i<memberList.size();i++){
                 talkCountMap.put(memberList.get(i),0);
+                killCountMap.put(memberList.get(i),0);
+                killUserListMap.put(memberList.get(i),"");
             }
             br = new BufferedReader(new FileReader(txt));
 
+
+            //대화횟수저장
             while((line=br.readLine()) != null) {
+
+                if(line.indexOf("나갔습니다")>=0){
+                    killCountMap.put(killUser,killCountMap.get(killUser)+1);
+                    int a=line.indexOf("님이");
+                    if(killUserListMap.get(killUser).length()>0){
+                        killUserListMap.put(killUser, killUserListMap.get(killUser) +" ,"  +  line.substring(0,a+1));
+                    }else{
+                        killUserListMap.put(killUser,killUserListMap.get(killUser) + line.substring(0,a+1));
+                    }
+
+                }
 
                 for(int i=0;i<memberList.size();i++){
                     if(line.indexOf(memberList.get(i))>=0){
                         talkCountMap.put(memberList.get(i),talkCountMap.get(memberList.get(i))+1);
                         totalLine++;
+                        killUser = memberList.get(i);
                     }
                 }
             }
@@ -62,23 +82,43 @@ public class Main {
             memberList.sort(Comparator.naturalOrder());
 
             //대화내용 순으로 talkCount 정렬
-            List<Map.Entry<String, Integer>> entryList = new LinkedList<>(talkCountMap.entrySet());
-            entryList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+            List<Map.Entry<String, Integer>> tokeList = new LinkedList<>(talkCountMap.entrySet());
+            tokeList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+            //사용자를 쫓아낸 순으로 killCount 정렬
+            List<Map.Entry<String, Integer>> killList = new LinkedList<>(killCountMap.entrySet());
+            killList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
 
 //            for(int i=0;i<memberList.size();i++){
 //                System.out.println("### " + memberList.get(i) + " : " + talkCount.get(memberList.get(i)));
 //            }
 
             int rank=1;
+            double jibun=0;
+            System.out.println("### 사용자를 쫓아낸 순위 ###");
+            for(Map.Entry<String, Integer> entry : killList){
+                if(entry.getValue()>0) {
+                    System.out.println(rank + "위. " + entry.getKey() + " : " + entry.getValue());
+                    System.out.println("내보낸 사용자 목록 : " + killUserListMap.get(entry.getKey()));
+                }
+                rank++;
+            }
 
+
+            System.out.println();
+            rank =1;
             System.out.println("### 말 많은 순위 ###");
-            for(Map.Entry<String, Integer> entry : entryList){
+            for(Map.Entry<String, Integer> entry : tokeList){
                 System.out.println(rank +"위. " + entry.getKey() + " : " + entry.getValue() + " ("+getPercent(totalLine,entry.getValue()) + "%)");
+                if(rank<=5){
+                    jibun = jibun+getPercent(totalLine,entry.getValue());
+                }
                 rank++;
             }
             //System.out.println(stringBuffer);
             //System.out.println("### 아 : " + countA);
             System.out.println("### 총 채팅 횟수 : " + totalLine);
+            System.out.println("### 1위부터 5위까지의 채팅 지분율 : " + jibun);
             System.out.println("### 대화를 한 사용자 수: " + memberList.size());
             System.out.println("### 대화를 한 사용자 목록: " + memberList);
         } catch (FileNotFoundException e) {
